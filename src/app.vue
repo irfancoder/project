7e1a93488b1e173c8547ca884a0388f4dee6d144
irfanismail <irfan.ismail96@gmail.com>
2022-07-20 22:00:41 +0800

    first commit

---
+++ src/app.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        Hello Krenovator 
+        <button @click="clickMe('whats up')">Print</button>
+    </div>
+</template>
+
+<script>
+    import { print } from 'library'
+    
+    export default {
+        mounted () {
+            print('Hello Krenovator')
+        },
+        methods: {
+            clickMe() {
+                print('Whats up')
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
